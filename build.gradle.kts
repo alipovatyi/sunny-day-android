b2d467706ec45a6698f0220c1fbcea7b30e075fb
Artem Lipovatyi <artemlipovatyi@gmail.com>
2023-03-16 16:52:33 +0100

    Set up Detekt plugin (#2)
    * Update project icon
    
    * Set up Detekt plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,23 @@
+import dev.arli.gradle.applyPlugin
+
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.detekt) apply true
 }
 
-allprojects {}
+allprojects {
+    applyPlugin(rootProject.libs.plugins.detekt)
+
+    dependencies {
+        detektPlugins(rootProject.libs.detekt.formatting)
+    }
+
+    tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+        autoCorrect = true
+
+        source(layout.projectDirectory.asFileTree.matching { include("**/*.kts") })
+    }
+}
