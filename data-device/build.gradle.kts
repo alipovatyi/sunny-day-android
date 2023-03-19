a4ab66942490887924dbac2e7446aa5c18a1dc0a
Artem Lipovatyi <artemlipovatyi@gmail.com>
2023-03-19 22:10:36 +0100

    Inject FusedLocationProviderClient

--- data-device/build.gradle.kts
+++ data-device/build.gradle.kts
@@ -2,6 +2,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    kotlin("kapt")
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -12,6 +14,15 @@ android {
     }
 }
 
+kapt {
+    correctErrorTypes = true
+}
+
 dependencies {
     implementation(project(":data-location"))
+
+    implementation(libs.kotlin.coroutines.playServices)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+    implementation(libs.playServices.location)
 }
