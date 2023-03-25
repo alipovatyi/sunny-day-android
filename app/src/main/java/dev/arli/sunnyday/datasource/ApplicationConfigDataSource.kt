45cbbd894853ddc6c3269a3b287747e3ea2478b0
Artem Lipovatyi <artemlipovatyi@gmail.com>
2023-03-25 02:19:57 +0100

    Fix injecting ApplicationConfigDataSource

--- app/src/main/java/dev/arli/sunnyday/datasource/ApplicationConfigDataSource.kt
+++ app/src/main/java/dev/arli/sunnyday/datasource/ApplicationConfigDataSource.kt
@@ -2,8 +2,9 @@ package dev.arli.sunnyday.datasource
 
 import dev.arli.sunnyday.data.config.datasource.ConfigDataSource
 import dev.arli.sunnyday.BuildConfig
+import javax.inject.Inject
 
-internal class ApplicationConfigDataSource : ConfigDataSource {
+internal class ApplicationConfigDataSource @Inject constructor() : ConfigDataSource {
 
     override val apiUrl: String
         get() = BuildConfig.API_URL
