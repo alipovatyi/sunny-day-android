b2435d9b538b5f75201b043d12eec2736a4d3f1c
Artem Lipovatyi <artemlipovatyi@gmail.com>
2023-04-02 02:21:18 +0200

    Fix DeviceDateTimeDataSource injecting

--- data-device/src/main/kotlin/dev/arli/sunnyday/data/device/datasource/DeviceDateTimeDataSource.kt
+++ data-device/src/main/kotlin/dev/arli/sunnyday/data/device/datasource/DeviceDateTimeDataSource.kt
@@ -3,8 +3,9 @@ package dev.arli.sunnyday.data.device.datasource
 import dev.arli.sunnyday.data.common.datasource.DateTimeDataSource
 import java.time.LocalDate
 import java.time.LocalDateTime
+import javax.inject.Inject
 
-internal class DeviceDateTimeDataSource : DateTimeDataSource {
+internal class DeviceDateTimeDataSource @Inject constructor() : DateTimeDataSource {
 
     override fun currentLocalDate(): LocalDate {
         return LocalDate.now()
